eac6e8e09bbaf04d3c875c7b06925781fa67e35b
AJ P <10paineaj@gmail.com>
2021-03-21 15:33:56 -0600

    Add functionality to write output file

--- PyBank/main.py
+++ PyBank/main.py
@@ -50,13 +50,26 @@ with open(csvpath) as csvfile:
 #Calculate Average Chanage
     Average_Change = round(Running_Profit_Change / (Total_Months-1), 2)
 # #Print OutPut Analysis
-    print("Financial Analysis")
-    print("------------------------")
-    print(f"Total Months: {Total_Months}")
-    print(f"Total: ${Total}")
-    print(f"Average Change: ${Average_Change}")
-    print(f"Greatest Increase in Profits: {Max_Profit_Change_Month} (${Max_Profit_Change})")
-    print(f"Greatest Decrease in Profits: {Min_Profit_Change_Month} (${Min_Profit_Change})")
+    print('Financial Analysis')
+    print('------------------------')
+    print(f'Total Months: {Total_Months}')
+    print(f'Total: ${Total}')
+    print(f'Average Change: ${Average_Change}')
+    print(f'Greatest Increase in Profits: {Max_Profit_Change_Month} (${Max_Profit_Change})')
+    print(f'Greatest Decrease in Profits: {Min_Profit_Change_Month} (${Min_Profit_Change})')
+
+#write output file
+output_path = os.path.join('Analysis', 'PyBank_Output_AJP.txt' )
+
+with open(output_path, 'w', newline='') as csvfile:
+    csvwriter = csv.writer(csvfile, delimiter=',')
+    csvwriter.writerow(['Financial Analysis'])
+    csvwriter.writerow(['------------------------'])
+    csvwriter.writerow([f'Total Months: {Total_Months}'])
+    csvwriter.writerow([f'Total: ${Total}'])
+    csvwriter.writerow([f'Average Change: ${Average_Change}'])
+    csvwriter.writerow([f'Greatest Increase in Profits: {Max_Profit_Change_Month} (${Max_Profit_Change})'])
+    csvwriter.writerow([f'Greatest Decrease in Profits: {Min_Profit_Change_Month} (${Min_Profit_Change})'])
 
 
 
