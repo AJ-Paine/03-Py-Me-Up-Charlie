0191d85b962e59f30e7a490c37e9fdae4436f05d
AJ P <10paineaj@gmail.com>
2021-03-25 18:28:23 -0600

    Add finishing touches and output functionality to PyPoll/main.py

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -20,13 +20,8 @@ with open(csvpath) as csvfile:
     for voter in pypoll_data:
         #Find total votes
         Total_Votes += 1
-        #Find candidate list
-        if voter[2] in Candidate_List:
-            continue
-        else:
-            Candidate_List.append(voter[2])
         #Find number of votes for Khan
-        if voter[2] == 'Kahn':
+        if voter[2] == 'Khan':
             Khan_Votes += 1
         #Find number of votes for Correy
         if voter[2] == 'Correy':
@@ -37,35 +32,66 @@ with open(csvpath) as csvfile:
         #Find number of votes for Correy
         if voter[2] == "O'Tooley":
             OTooley_Votes += 1
+        #Find candidate list
+        if voter[2] in Candidate_List:
+            continue
+        else:
+             Candidate_List.append(voter[2])
     
     
-    #Calcuate percentage of votes
-    kahn_perc = Khan_Votes/Total_Votes
-    Correy_perc = Correy_Votes/Total_Votes
-    Li_perc = Li_Votes/Total_Votes
-    OTooley_perc = OTooley_Votes/Total_Votes
-    
+    #Calcuate percentage of votes and format to 3 decimal points
+    Khan_perc = format(Khan_Votes/Total_Votes*100, ".3f")
+    Correy_perc = format(Correy_Votes/Total_Votes*100, ".3f")
+    Li_perc = format(Li_Votes/Total_Votes*100, ".3f")
+    OTooley_perc = format(OTooley_Votes/Total_Votes*100, ".3f")
+
+    if Khan_perc > Correy_perc and Khan_perc > Li_perc and Khan_perc > OTooley_perc:
+        winner = "Khan"
+    elif Correy_perc > Li_perc and Correy_perc > OTooley_perc:
+        winner = "Correy"
+    elif Li_perc > OTooley_perc:
+        winner = "Li"
+    else:
+        winner = "O'Tooley"
+
+    #Create Dictionary
+    Election_Summary = dict()
+    Election_Summary["Candidate"] = Candidate_List
+    Election_Summary["Percentage of Vote"] = [Khan_perc, Correy_perc, Li_perc, OTooley_perc]
+    Election_Summary["Number of Votes"] = [Khan_Votes, Correy_Votes, Li_Votes, OTooley_Votes]
+
+    #print(Election_Summary)
     #Print Results
+    #print(Candidate_List)
     print('Election Results')
-    print('---------------------')
+    print('---------------------------')
     print(f'Total Votes: {Total_Votes}')
-    print('---------------------')
-    print(f'Khan: {Khan_Votes}')
-    print(Correy_Votes)
-    print(Li_Votes)
-    print(OTooley_Votes)
-        
-# #Create Candidate List
-# Candidate_List = []
-# for voter in pypoll_data:
-#     #Candidate = str(row[2])
-#     if row[2] in Candidate_List:
-#         continue
-#     else:
-#         Candidate_List.append(row[2])
-#     print(Candidate_List)
-#     print(voter)
+    print('---------------------------')
+
+    #Print via Dictionary
+    print(f'{Election_Summary["Candidate"][0]}: {Election_Summary["Percentage of Vote"][0]}% ({Election_Summary["Number of Votes"][0]})')
+    print(f'{Election_Summary["Candidate"][1]}: {Election_Summary["Percentage of Vote"][1]}% ({Election_Summary["Number of Votes"][1]})')   
+    print(f'{Election_Summary["Candidate"][2]}: {Election_Summary["Percentage of Vote"][2]}% ({Election_Summary["Number of Votes"][2]})')
+    print(f'{Election_Summary["Candidate"][3]}: {Election_Summary["Percentage of Vote"][3]}% ({Election_Summary["Number of Votes"][3]})') 
+    print('---------------------------')
+    print(f'Winner: {winner}')
+    print('---------------------------')
 
 
+    #write output file
+    output_path = os.path.join('Analysis', 'PyPoll_Output_AJP.txt')
+    with open(output_path, 'w', newline='') as output:
+        csvwriter = csv.writer(output, delimiter=',')
+        csvwriter.writerow(['Election Results'])
+        csvwriter.writerow(['---------------------------'])
+        csvwriter.writerow([f'Total Votes: {Total_Votes}'])
+        csvwriter.writerow(['---------------------------'])
+        csvwriter.writerow([f'{Election_Summary["Candidate"][0]}: {Election_Summary["Percentage of Vote"][0]}% ({Election_Summary["Number of Votes"][0]})'])
+        csvwriter.writerow([f'{Election_Summary["Candidate"][1]}: {Election_Summary["Percentage of Vote"][1]}% ({Election_Summary["Number of Votes"][1]})'])
+        csvwriter.writerow([f'{Election_Summary["Candidate"][2]}: {Election_Summary["Percentage of Vote"][2]}% ({Election_Summary["Number of Votes"][2]})'])
+        csvwriter.writerow([f'{Election_Summary["Candidate"][3]}: {Election_Summary["Percentage of Vote"][3]}% ({Election_Summary["Number of Votes"][3]})'])
+        csvwriter.writerow(['---------------------------'])
+        csvwriter.writerow([f'Winner: {winner}'])
+        csvwriter.writerow(['---------------------------'])
         
 
